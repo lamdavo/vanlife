75cb9d33f11aeae3b51fcfd0c6f92d6fc8e7465a
Lam <vo2022@protonmail.com>
2023-05-15 21:11:57 -0700

    added vans, useFetchVan, Van Tile Css

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,32 @@
+import { useFetchVans } from "../fetchVans";
+
 const Vans = () => {
-  return <div>Vans</div>;
+  const { loading, vans } = useFetchVans();
+
+  if (loading) return <div>Loading...</div>;
+
+  const vanElements = vans.map((van) => {
+    const { id, title, description, price, tier, img } = van;
+    return (
+      <div key={id} className="van-tile">
+        <img src={img} alt="van image" />
+        <div className="van-info">
+          <h3>{title}</h3>
+          <p>
+            ${price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${tier} selected`}>{tier}</i>
+      </div>
+    );
+  });
+
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vanElements}</div>
+    </div>
+  );
 };
 export default Vans;
